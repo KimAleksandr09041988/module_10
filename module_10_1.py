0fce8457c303b2f9d9e7d77978fa49c5f0a76b0c
Aleksandr Kim <112456407+KimAleksandr09041988@users.noreply.github.com>
2024-12-10 12:23:21 +0800

    Update module_10_1.py

--- module_10_1.py
+++ module_10_1.py
@@ -14,21 +14,23 @@ write_words(30, 'example2.txt')
 write_words(200, 'example3.txt')
 write_words(100, 'example4.txt')
 time_2 = time()
-print((time_2 - time_1)*1000)
+print(time_2 - time_1)
 thread_1 = Thread(target=write_words, args=(10,'example5.txt'))
 thread_2 = Thread(target=write_words, args=(30,'example6.txt'))
 thread_3 = Thread(target=write_words, args=(200,'example7.txt'))
 thread_4 = Thread(target=write_words, args=(100,'example8.txt'))
 
 time_3 = time()
+
 thread_1.start()
-
 thread_2.start()
-
 thread_3.start()
-
 thread_4.start()
+
+thread_1.join()
+thread_2.join()
+thread_3.join()
 thread_4.join()
 
 time_4 = time()
-print((time_4 - time_3)*1000)
+print(time_4 - time_3)
